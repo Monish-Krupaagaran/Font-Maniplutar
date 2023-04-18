a6a4435c601de09cb262b3ac2c2b8ab2bc9f1683
Monish-Krupaagaran <104307612+Monish-Krupaagaran@users.noreply.github.com>
2023-04-18 21:04:06 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,52 @@
+noseX = 0
+noseY = 0
+
+rightX = 0
+leftX = 0
+
+difference = 0
+
+function setup() {
+
+    canvas = createCanvas(550, 550)
+    canvas.position(800, 200)
+
+    video = createCapture(VIDEO)
+    video.size(500, 500)
+
+    posenet = ml5.poseNet(video, model_loaded)
+    posenet.on("pose", gotposes)
+}
+
+function model_loaded() {
+
+    console.log("model is loaded")
+}
+
+function gotposes(result) {
+
+    if(result.length > 0) {
+
+        console.log(result);
+
+        noseX = result[0].pose.nose.x 
+        nosey = result[0].pose.nose.y 
+
+        leftX = result[0].pose.leftWrist.x
+        rightX = result[0].pose.rightWrist.x 
+
+        difference = floor(leftX - rightX)
+    }
+}
+
+function draw() {
+
+    background("black")
+
+    stroke("white")
+    fill("white")
+    textSize(difference)
+    text("Monish.S", 250,250)
+
+    document.getElementById("px").innerHTML = difference + " px"
+}
